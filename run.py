e649ce01643984fa15f0628bbb12634871e08298
Ritul Bharati <ritul.pravash@gmail.com>
2024-12-04 23:12:00 +0530

    final changes

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO('./models/yolov8n.pt')  # Use a pre-trained YOLO model
 
 # Train on your dataset
-model.train(data='./data.yaml', epochs=10, imgsz=640, batch=16)
+model.train(data='./data.yaml', epochs=30, imgsz=640, batch=16)
